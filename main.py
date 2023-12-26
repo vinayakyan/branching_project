d74ac44618852ad77afbddb0f2479699556b2aeb
vinayak <vcharms@gmail.com>
2023-12-26 19:17:42 +0530

    Modified the content of main.py in dev_branch

--- main.py
+++ main.py
@@ -6,4 +6,11 @@ for i in range(0,50,2):
 
 print("Addition of Even numbers is :", add)
 
+odd_add = 0
+
+for i in range(1,50,2):
+    odd_add += i
+
+print("Additon of odd numbers is :", odd_add)
+
 print("Program Ends")
